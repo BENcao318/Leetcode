05d3d3bcf485a235ea230a4e11ba28a9d4f605a1
Benyuan Cao <cby204@gmail.com>
2023-03-05 21:23:01 -0500

    Add answer to the search in rotated sorted array question

--- Binary Search/searchInRotatedSortedArray.js
+++ Binary Search/searchInRotatedSortedArray.js
@@ -9,14 +9,16 @@ var search = function (nums, target) {
   while (left <= right) {
     const mid = Math.floor((left + right) / 2)
     if (target === nums[mid]) return mid
-
+    //left sorted portion
     if (nums[left] <= nums[mid]) {
       if (target > nums[mid] || target < nums[left]) {
         left = mid + 1
       } else {
         right = mid - 1
       }
-    } else {
+    }
+    //right sorted portion
+    else {
       if (target < nums[mid] || target > nums[right]) {
         right = mid - 1
       } else {
