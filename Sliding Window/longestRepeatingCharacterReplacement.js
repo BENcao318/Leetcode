b855b74ab457d2e70477a02d69c8b4894370ce32
Benyuan Cao <cby204@gmail.com>
2023-03-14 21:20:28 -0400

    Add the answer to the Permutation In String question.

--- Sliding Window/longestRepeatingCharacterReplacement.js
+++ Sliding Window/longestRepeatingCharacterReplacement.js
@@ -1,51 +1,47 @@
 /**
- * https://leetcode.com/problems/longest-repeating-character-replacement/
- * Time O(((N + 26) * N) * (M - N)) | Space O(1)
  * @param {string} s
  * @param {number} k
  * @return {number}
  */
 var characterReplacement = function (s, k) {
-  let [left, right, longest, max] = new Array(4).fill(0)
-  const frequencyMap = new Array(26).fill(0)
+  let [left, right, max] = new Array(4).fill(0)
+  let frequencyMap = {}
 
   while (right < s.length) {
-    const count = addRightFrequency(s, right, frequencyMap)
+    addRightFrequency(s, right, frequencyMap)
 
-    longest = Math.max(longest, count)
-
-    let window = right - left + 1
-    const canSlide = k < window - longest
-    if (canSlide) {
+    let count = getHighestChar(frequencyMap)
+    const windowLength = right - left + 1
+    if (windowLength - count <= k) {
+      max = Math.max(windowLength, max)
+    } else {
       subtractLeftFrequency(s, left, frequencyMap)
       left++
     }
-
-    window = right - left + 1
-    max = Math.max(max, window)
-
     right++
   }
 
   return max
 }
 
-const addRightFrequency = (s, right, map) => {
+const addRightFrequency = (s, right, frequencyMap) => {
   const char = s[right]
-  const index = getCode(char)
 
-  map[index]++
-
-  return map[index]
+  if (!frequencyMap[char]) {
+    frequencyMap[char] = 1
+  } else {
+    frequencyMap[char] += 1
+  }
 }
 
-const subtractLeftFrequency = (s, left, map) => {
+const subtractLeftFrequency = (s, left, frequencyMap) => {
   const char = s[left]
-  const index = getCode(char)
-
-  map[index]--
-
-  return map[index]
+  frequencyMap[char] -= 1
 }
 
-const getCode = (char) => char.charCodeAt(0) - 'A'.charCodeAt(0)
+const getHighestChar = (frequencyMap) => {
+  let arr = Object.values(frequencyMap)
+  let max = Math.max(...arr)
+
+  return max || 0
+}
