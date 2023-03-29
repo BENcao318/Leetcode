a969dfb0f95185ce7e078a4cbc8c5a03e09c7099
Benyuan Cao <cby204@gmail.com>
2023-03-28 21:26:14 -0400

    Add the alternative answer to the question: Maximum Depth Of Binary Tree.

--- binaryTree/maximumDepthOfBinaryTree.js
+++ binaryTree/maximumDepthOfBinaryTree.js
@@ -4,18 +4,33 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function (root) {
-  const isBaseCase = root === null
-  if (isBaseCase) return 0
+// var maxDepth = function (root) {
+//   const isBaseCase = root === null
+//   if (isBaseCase) return 0
 
-  return dfs(root)
-}
+//   return dfs(root)
+// }
 
-const dfs = (root) => {
-  const left = maxDepth(root.left)
-  const right = maxDepth(root.right)
+// const dfs = (root) => {
+//   const left = maxDepth(root.left)
+//   const right = maxDepth(root.right)
 
-  const height = Math.max(left, right)
+//   const height = Math.max(left, right)
+//   return height + 1
+// }
+const maxDepth = (root) => {
+  if (!root) return 0
+  let queue = [root]
+  let depth = 0
+  while (queue.length > 0) {
+    let levelSize = queue.length
+    for (let i = 0; i < levelSize; i++) {
+      let node = queue.shift()
 
-  return height + 1
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+    depth++
+  }
+  return depth
 }
