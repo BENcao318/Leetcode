4bd160b48950147261e14ffa0976e7fe69c26245
Benyuan Cao <cby204@gmail.com>
2023-03-29 21:16:28 -0400

    Add the alternative answer to the question: Invert Binary Tree.

--- binaryTree/invertBinaryTree.js
+++ binaryTree/invertBinaryTree.js
@@ -10,19 +10,38 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = (root) => {
-  const isBaseCase = root === null
-  if (isBaseCase) return root
+// var invertTree = (root) => {
+//   const isBaseCase = root === null
+//   if (isBaseCase) return root
 
-  return dfs(root)
-}
+//   return dfs(root)
+// }
 
-const dfs = (root) => {
-  const left = invertTree(root.left)
-  const right = invertTree(root.right)
+// const dfs = (root) => {
+//   const left = invertTree(root.left)
+//   const right = invertTree(root.right)
 
-  root.left = right
-  root.right = left
+//   root.left = right
+//   root.right = left
+
+//   return root
+// }
+
+const invertTree = (root) => {
+  if (!root) return null
+
+  let queue = [root]
+
+  while (queue.length > 0) {
+    let node = queue.shift()
+
+    let temp = node.left
+    node.left = node.right
+    node.right = temp
+
+    if (node.left) queue.push(node.left)
+    if (node.right) queue.push(node.right)
+  }
 
   return root
 }
