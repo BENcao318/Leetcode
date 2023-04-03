36d0006c738f4e42610c70da0ad634392af9284a
Benyuan Cao <cby204@gmail.com>
2023-04-02 21:01:53 -0400

    Add the alternative answer to the question Valid Anagram.

--- Arrays/validAnagram.js
+++ Arrays/validAnagram.js
@@ -3,37 +3,62 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-  //Frequency Counter
-  // if(s.length !== t.length) return false
+// var isAnagram = function(s, t) {
+//   //Frequency Counter
+//   // if(s.length !== t.length) return false
 
-  // const dict = {}
-  // let count = 0
+//   // const dict = {}
+//   // let count = 0
 
-  // for(let i = 0; i < s.length; i++) {
-  //     if (!dict[s[i]]) {
-  //         dict[s[i]] = 1
-  //     } else {
-  //         dict[s[i]] += 1
-  //     }
-  // }
+//   // for(let i = 0; i < s.length; i++) {
+//   //     if (!dict[s[i]]) {
+//   //         dict[s[i]] = 1
+//   //     } else {
+//   //         dict[s[i]] += 1
+//   //     }
+//   // }
 
-  // for(let i = 0; i < t.length; i++) {
-  //     if(dict[t[i]] && dict[t[i]] !== 0) {
-  //         dict[t[i]] -= 1
-  //         count++
-  //     } 
-  // }
+//   // for(let i = 0; i < t.length; i++) {
+//   //     if(dict[t[i]] && dict[t[i]] !== 0) {
+//   //         dict[t[i]] -= 1
+//   //         count++
+//   //     }
+//   // }
 
-  // return count === s.length
+//   // return count === s.length
 
-  //Hash Sort (O(N*LogN))
+//   //Hash Sort (O(N*LogN))
 
-  if(s.length !== t.length) return false
+//   if(s.length !== t.length) return false
 
-  return reorder(s) === reorder(t)
-};
+//   return reorder(s) === reorder(t)
+// };
 
-const reorder = (string) => {
-  return string.split("").sort().join("")
+// const reorder = (string) => {
+//   return string.split("").sort().join("")
+// }
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false
+
+  const freqS = new Array(26).fill(0)
+  const freqT = new Array(26).fill(0)
+
+  for (let i = 0; i < s.length; i++) {
+    freqS[s.charCodeAt(i) - 97]++
+    freqT[t.charCodeAt(i) - 97]++
+  }
+
+  for (let i = 0; i < 26; i++) {
+    if (freqS[i] !== freqT[i]) {
+      return false
+    }
+  }
+
+  return true
 }
