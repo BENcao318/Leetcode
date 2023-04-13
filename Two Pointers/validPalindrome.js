f4750b01dc94d844b6d3a23cc53bdbfc523cf101
Benyuan Cao <cby204@gmail.com>
2023-04-12 21:52:54 -0400

    Good Solution for the Valid Palindrome

--- Two Pointers/validPalindrome.js
+++ Two Pointers/validPalindrome.js
@@ -26,3 +26,30 @@ var isPalindrome = function (s) {
 
   return true
 }
+
+
+View on Github
+/**
+ * Array - Filter && Clone && Reverse
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/valid-palindrome/
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    if (!s.length) return true;
+    
+    const alphaNumeric = filterAlphaNumeric(s);/* Time O(N) | Space O(N) */
+    const reversed = reverse(alphaNumeric);    /* Time O(N) | Space O(N) */
+    
+    return alphaNumeric === reversed;
+};
+
+const filterAlphaNumeric = (s, nonAlphaNumeric = new RegExp('[^a-z0-9]','gi')) => s
+    .toLowerCase()               /* Time O(N) | Space O(N) */
+    .replace(nonAlphaNumeric, '')/* Time O(N) | Space O(N) */
+
+const reverse = (s) => s
+    .split('')/* Time O(N) | Space O(N) */
+    .reverse()/* Time O(N) | Space O(N) */
+    .join('');/* Time O(N) | Space O(N) */
