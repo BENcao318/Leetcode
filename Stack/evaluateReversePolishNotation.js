003d25d2d8c74bd751bd574b6f1616cfad4785ed
Benyuan Cao <cby204@gmail.com>
2023-04-18 21:59:33 -0400

    Solutions for Evaluate Reverse Polish Notation.

--- Stack/evaluateReversePolishNotation.js
+++ Stack/evaluateReversePolishNotation.js
@@ -94,3 +94,41 @@ const operate = (a, b, operator) => {
       return Math.trunc(a / b)
   }
 }
+
+/**
+ * https://leetcode.com/problems/evaluate-reverse-polish-notation
+ * Time O(N) | Space(N)
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens, stack = []) {
+  for (const char of tokens) {
+    /* Time O(N) */
+    const isOperation = char in OPERATORS
+    if (isOperation) {
+      const value = performOperation(char, stack)
+
+      stack.push(value) /* Space O(N) */
+
+      continue
+    }
+
+    stack.push(Number(char)) /* Space O(N) */
+  }
+
+  return stack.pop()
+}
+
+var OPERATORS = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => Math.trunc(a / b),
+}
+
+var performOperation = (char, stack) => {
+  const [rightNum, leftNum] = [stack.pop(), stack.pop()]
+  const operation = OPERATORS[char]
+
+  return operation(leftNum, rightNum)
+}
