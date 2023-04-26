b7e60846a3132ada272ccdaf52f130d74cccefd9
Benyuan Cao <cby204@gmail.com>
2023-04-25 21:58:54 -0400

    Solution for reverse Liked List.

--- Linked List/reverseLinkedList.js
+++ Linked List/reverseLinkedList.js
@@ -22,3 +22,24 @@ var reverseList = function (head) {
 
   return prev
 }
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ * Time O(N) | Space O(1)
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let [prev, curr, next] = [null, head, null]
+
+  while (curr) {
+    /* Time O(N) */
+    next = curr.next
+    curr.next = prev
+
+    prev = curr
+    curr = next
+  }
+
+  return prev
+}
